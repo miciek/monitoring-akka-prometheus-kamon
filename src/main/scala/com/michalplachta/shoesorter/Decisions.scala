062ca8d5d60c5c6ffc74ec4e52cb6763b18e7eba
miciek <miciek@gmail.com>
2016-03-23 19:51:17 +0100

    Add different delay per junction and change ports

--- src/main/scala/com/michalplachta/shoesorter/Decisions.scala
+++ src/main/scala/com/michalplachta/shoesorter/Decisions.scala
@@ -3,8 +3,17 @@ import com.michalplachta.shoesorter.Domain.{Container, Junction}
 
 object Decisions {
   def whereShouldContainerGo(junction: Junction, container: Container): String = {
-    Thread.sleep(5) // just to simulate resource hunger
-    val seed = util.Random.nextInt(10000)
-    s"CVR_${junction.id}_$seed"
+    if(junction.id > 999) {
+      Thread.sleep(junction.id)
+    } else {
+      Thread.sleep(5) // just to simulate resource hunger
+    }
+
+    if(junction.id != 666) {
+      val seed = util.Random.nextInt(10000)
+      s"CVR_${junction.id}_$seed"
+    } else {
+      throw new NullPointerException("undefined is not a function <3")
+    }
   }
 }
