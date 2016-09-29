d36a9fedfb071a9cd9e3bf3ba215c1abb668d0fc
miciek <miciek@gmail.com>
2016-09-29 15:31:24 +0200

    Configure prometheus correctly and add 2 simple counters

--- src/main/scala/com/michalplachta/shoesorter/Decisions.scala
+++ src/main/scala/com/michalplachta/shoesorter/Decisions.scala
@@ -1,19 +1,30 @@
 package com.michalplachta.shoesorter
 import com.michalplachta.shoesorter.Domain.{Container, Junction}
+import kamon.Kamon
 
 object Decisions {
+  val firstJunctionId = 1
+  val lastJunctionId = 5
+
   def whereShouldContainerGo(junction: Junction, container: Container): String = {
-    if(junction.id > 999) {
-      Thread.sleep(junction.id)
-    } else {
-      Thread.sleep(5) // just to simulate resource hunger
+    Thread.sleep(5) // just to simulate resource hunger
+
+    if(junction.id == firstJunctionId) {
+      Kamon.metrics.counter(
+        "containers_in_total",
+        Map("junctionId" -> junction.id.toString)
+      ).increment()
     }
 
-    if(junction.id != 666) {
+    if(junction.id == lastJunctionId) {
+      Kamon.metrics.counter(
+        "containers_out_total",
+        Map("junctionId" -> junction.id.toString)
+      ).increment()
+      "OUT"
+    } else {
       val seed = util.Random.nextInt(10000)
       s"CVR_${junction.id}_$seed"
-    } else {
-      throw new NullPointerException("undefined is not a function <3")
     }
   }
 }
