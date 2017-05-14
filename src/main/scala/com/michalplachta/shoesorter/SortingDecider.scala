2d6014a8f6069fac85329925d379ca1103c41d7a
miciek <miciek@gmail.com>
2017-05-14 15:16:13 +0200

    Remove Kamon gauges

--- src/main/scala/com/michalplachta/shoesorter/SortingDecider.scala
+++ src/main/scala/com/michalplachta/shoesorter/SortingDecider.scala
@@ -2,9 +2,7 @@ package com.michalplachta.shoesorter
 
 import akka.actor.{Actor, ActorLogging, Props}
 import akka.cluster.sharding.ShardRegion
-import com.michalplachta.shoesorter.Domain.Junction
 import com.michalplachta.shoesorter.Messages.{Go, WhereShouldIGo}
-import kamon.Kamon
 
 object SortingDecider {
   def props = Props[SortingDecider]
@@ -23,21 +21,10 @@ object SortingDecider {
 }
 
 class SortingDecider extends Actor with ActorLogging {
-  var totalContainers = 0
-
   def receive = {
     case WhereShouldIGo(junction, container) =>
       val decision = Decisions.whereShouldContainerGo(junction, container)
       log.debug(s"Decision: $decision")
-      recordJunctionMetrics(junction)
       sender ! Go(decision)
   }
-
-  def recordJunctionMetrics(junction: Junction): Unit = {
-    if(junction.id == 1) totalContainers = totalContainers + 1
-    if(junction.id == 5) totalContainers = totalContainers - 1
-    Kamon.metrics.gauge("containers_total")(0L).record(totalContainers)
-  }
 }
-
-
